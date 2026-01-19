21e537cd6fbd06c42af2091921c69009f203af68
ccoop129 <chriscoop129@gmail.com>
2026-01-19 17:11:35 -0500

    csv debug

--- data/scripts/count_noun_chunks_entities.py
+++ data/scripts/count_noun_chunks_entities.py
@@ -454,7 +454,7 @@ ENTITIES_TO_SKIP = {
 
 # Load data
 print("Loading CH_RU_prime.csv...")
-df = pd.read_csv(os.path.join(data_dir, "CH_RU_prime.csv"))
+df = pd.read_csv(os.path.join(data_dir, "CH_RU_prime.csv"), encoding='utf-16')
 
 # Process by country
 countries = ['China', 'Russia']
